1968d70435bf7939cffed255d4a27da111d1e78d
Pasgen-Manley <pasgen95@gmail.com>
2021-08-30 12:36:22 +0800

    Asscociates set up in index.js

--- models/index.js
+++ models/index.js
@@ -3,14 +3,25 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { CallTracker } = require('assert/strict');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, { 
+  through: ProductTag 
+});
+
 
 module.exports = {
   Product,
